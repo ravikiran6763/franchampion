989bac37e5fff0cba7bc140ed53516cb3a73d787
ravikiran <ravikiran6763@gmail.com>
2020-03-12 13:06:26 +0530

    No vehicle scenario updated

--- pdfapp/urls.py
+++ pdfapp/urls.py
@@ -4,11 +4,17 @@ from pdfapp import views
 
 urlpatterns = [
     url(r'^generate/',views.pdf_gen,name='pdf_gen'),
+    path('generate/', views.pdf_gen),
     url(r'^sendMail/', views.sendMail,name='sendMail'), 
     path('sendMail/', views.sendMail),
+    
     url(r'^template1/', views.template1,name='template1'), 
     path('template1/', views.template1),
+    
     url(r'^template2/', views.template2,name='template2'), 
     path('template2/', views.template2),
+    
+    url(r'^template3/', views.template3,name='template3'), 
+    path('template3/', views.template3),
     
 ]
